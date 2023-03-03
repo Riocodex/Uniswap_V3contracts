7c48bc8b2926f06402c8f98caaf068bf0fcb9ccb
Rio <onwukachibike@gmail.com>
2023-03-03 22:12:59 +0100

    updated

--- contracts/LiquidityExample.sol
+++ contracts/LiquidityExample.sol
@@ -1,17 +1,15 @@
-//SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity = 0.7.6;
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity =0.7.6;
 pragma abicoder v2;
 
 import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
 import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
-import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
-import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
-import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
+import '../libraries/TransferHelper.sol';
+import '../interfaces/INonfungiblePositionManager.sol';
+import '../base/LiquidityManagement.sol';
 
 contract LiquidityExamples is IERC721Receiver {
-
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
@@ -19,6 +17,7 @@ contract LiquidityExamples is IERC721Receiver {
 
     INonfungiblePositionManager public immutable nonfungiblePositionManager;
 
+    /// @notice Represents the deposit of an NFT
     struct Deposit {
         address owner;
         uint128 liquidity;
@@ -26,16 +25,16 @@ contract LiquidityExamples is IERC721Receiver {
         address token1;
     }
 
+    /// @dev deposits[tokenId] => Deposit
     mapping(uint256 => Deposit) public deposits;
 
     constructor(
-        INonfungiblePositionManager _nonfungiblePositionManager,
-        address _factory,
-        address _WETH9
-    ) PeripheryImmutableState(_factory, _WETH9) {
+        INonfungiblePositionManager _nonfungiblePositionManager
+    ) {
         nonfungiblePositionManager = _nonfungiblePositionManager;
     }
 
+    // Implementing `onERC721Received` so this contract can receive custody of erc721 tokens
     function onERC721Received(
         address operator,
         address,
@@ -43,7 +42,9 @@ contract LiquidityExamples is IERC721Receiver {
         bytes calldata
     ) external override returns (bytes4) {
         // get position information
+
         _createDeposit(operator, tokenId);
+
         return this.onERC721Received.selector;
     }
 
@@ -55,5 +56,4 @@ contract LiquidityExamples is IERC721Receiver {
         // operator is msg.sender
         deposits[tokenId] = Deposit({owner: owner, liquidity: liquidity, token0: token0, token1: token1});
     }
-
 }
