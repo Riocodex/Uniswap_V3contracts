71f43acf06f7d7e2ef9b5555adbc691a471c5a1f
Rio <onwukachibike@gmail.com>
2023-03-03 21:56:26 +0100

    added contract addresses'

--- contracts/LiquidityExample.sol
+++ contracts/LiquidityExample.sol
@@ -9,3 +9,12 @@ import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
+
+contract LiquidityExamples is IERC721Receiver {
+
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    uint24 public constant poolFee = 3000;
+    
+}
