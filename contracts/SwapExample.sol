c481eb6edba0336f5e980dbc32a32fc8646d52f5
Rio <onwukachibike@gmail.com>
2023-03-03 15:24:18 +0100

    added constructor

--- contracts/SwapExample.sol
+++ contracts/SwapExample.sol
@@ -11,4 +11,8 @@ contract SwapExamples{
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     uint24 public constant poolFee = 3000;
+
+    constructor (ISwapRouter _swapRouter){
+        swapRouter = _swapRouter;
+    }
 }
