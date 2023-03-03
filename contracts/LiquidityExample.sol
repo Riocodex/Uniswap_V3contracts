bf75311095d518af0911d5d53dbf96b5747038c5
Rio <onwukachibike@gmail.com>
2023-03-03 21:51:41 +0100

    added imports

--- contracts/LiquidityExample.sol
+++ contracts/LiquidityExample.sol
@@ -2,3 +2,10 @@
 pragma solidity = 0.7.6;
 pragma abicoder v2;
 
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
+import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
+import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+import '@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol';
