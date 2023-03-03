c9d95af4a10558c1281e53dd41edf296c45c6187
Rio <onwukachibike@gmail.com>
2023-03-03 15:07:01 +0100

    created contract and added dai,weth and usdc addresses

---
+++ contracts/SwapExample.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity = 0.7.6;
+pragma abicoder v2;
+
+import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
+
+contract SwapExamples{
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    
+}
