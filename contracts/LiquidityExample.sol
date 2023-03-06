31a4563a56c0e2702334a0290ed26de9054cb491
Rio <onwukachibike@gmail.com>
2023-03-06 21:09:54 +0100

    updated

--- contracts/LiquidityExample.sol
+++ contracts/LiquidityExample.sol
@@ -57,7 +57,8 @@ contract LiquidityExamples is IERC721Receiver {
         deposits[tokenId] = Deposit({owner: owner, liquidity: liquidity, token0: token0, token1: token1});
     }
 
-        /// @notice Calls the mint function defined in periphery, mints the same amount of each     token. For this example we are providing 1000 DAI and 1000 USDC in liquidity
+    /// @notice Calls the mint function defined in periphery, mints the same amount of each token.
+    /// For this example we are providing 1000 DAI and 1000 USDC in liquidity
     /// @return tokenId The id of the newly minted ERC721
     /// @return liquidity The amount of liquidity for the position
     /// @return amount0 The amount of token0
@@ -76,29 +77,33 @@ contract LiquidityExamples is IERC721Receiver {
         uint256 amount0ToMint = 1000;
         uint256 amount1ToMint = 1000;
 
-                // Approve the position manager
-                TransferHelper.safeApprove(DAI, address(nonfungiblePositionManager), amount0ToMint);
-                TransferHelper.safeApprove(USDC, address(nonfungiblePositionManager), amount1ToMint);
-        
-                INonfungiblePositionManager.MintParams memory params =
-                    INonfungiblePositionManager.MintParams({
-                        token0: DAI,
-                        token1: USDC,
-                        fee: poolFee,
-                        tickLower: TickMath.MIN_TICK,
-                        tickUpper: TickMath.MAX_TICK,
-                        amount0Desired: amount0ToMint,
-                        amount1Desired: amount1ToMint,
-                        amount0Min: 0,
-                        amount1Min: 0,
-                        recipient: address(this),
-                        deadline: block.timestamp
-                    });
-        
-                // Note that the pool defined by DAI/USDC and fee tier 0.3% must already be created and initialized in order to mint
-                (tokenId, liquidity, amount0, amount1) = nonfungiblePositionManager.mint(params);
+        // transfer tokens to contract
+        TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amount0ToMint);
+        TransferHelper.safeTransferFrom(USDC, msg.sender, address(this), amount1ToMint);
 
-                        // Create a deposit
+        // Approve the position manager
+        TransferHelper.safeApprove(DAI, address(nonfungiblePositionManager), amount0ToMint);
+        TransferHelper.safeApprove(USDC, address(nonfungiblePositionManager), amount1ToMint);
+
+        INonfungiblePositionManager.MintParams memory params =
+            INonfungiblePositionManager.MintParams({
+                token0: DAI,
+                token1: USDC,
+                fee: poolFee,
+                tickLower: TickMath.MIN_TICK,
+                tickUpper: TickMath.MAX_TICK,
+                amount0Desired: amount0ToMint,
+                amount1Desired: amount1ToMint,
+                amount0Min: 0,
+                amount1Min: 0,
+                recipient: address(this),
+                deadline: block.timestamp
+            });
+
+        // Note that the pool defined by DAI/USDC and fee tier 0.3% must already be created and initialized in order to mint
+        (tokenId, liquidity, amount0, amount1) = nonfungiblePositionManager.mint(params);
+
+        // Create a deposit
         _createDeposit(msg.sender, tokenId);
 
         // Remove allowance and refund in both assets.
@@ -115,15 +120,13 @@ contract LiquidityExamples is IERC721Receiver {
         }
     }
 
-        /// @notice Collects the fees associated with provided liquidity
+    /// @notice Collects the fees associated with provided liquidity
     /// @dev The contract must hold the erc721 token before it can collect fees
     /// @param tokenId The id of the erc721 token
     /// @return amount0 The amount of fees collected in token0
     /// @return amount1 The amount of fees collected in token1
     function collectAllFees(uint256 tokenId) external returns (uint256 amount0, uint256 amount1) {
-        // Caller must own the ERC721 position
-        // Call to safeTransfer will trigger `onERC721Received` which must return the selector else transfer will fail
-        nonfungiblePositionManager.safeTransferFrom(msg.sender, address(this), tokenId);
+        // Caller must own the ERC721 position, meaning it must be a deposit
 
         // set amount0Max and amount1Max to uint256.max to collect all fees
         // alternatively can set recipient to msg.sender and avoid another transaction in `sendToOwner`
@@ -141,26 +144,7 @@ contract LiquidityExamples is IERC721Receiver {
         _sendToOwner(tokenId, amount0, amount1);
     }
 
-            /// @notice Transfers funds to owner of NFT
-    /// @param tokenId The id of the erc721
-    /// @param amount0 The amount of token0
-    /// @param amount1 The amount of token1
-    function _sendToOwner(
-        uint256 tokenId,
-        uint256 amount0,
-        uint256 amount1
-    ) internal {
-        // get owner of contract
-        address owner = deposits[tokenId].owner;
-
-        address token0 = deposits[tokenId].token0;
-        address token1 = deposits[tokenId].token1;
-        // send collected fees to owner
-        TransferHelper.safeTransfer(token0, owner, amount0);
-        TransferHelper.safeTransfer(token1, owner, amount1);
-    }
-
-        /// @notice A function that decreases the current liquidity by half. An example to show how to call the `decreaseLiquidity` function defined in periphery.
+    /// @notice A function that decreases the current liquidity by half. An example to show how to call the `decreaseLiquidity` function defined in periphery.
     /// @param tokenId The id of the erc721 token
     /// @return amount0 The amount received back in token0
     /// @return amount1 The amount returned back in token1
@@ -188,27 +172,7 @@ contract LiquidityExamples is IERC721Receiver {
         _sendToOwner(tokenId, amount0, amount1);
     }
 
-        /// @notice Transfers funds to owner of NFT
-    /// @param tokenId The id of the erc721
-    /// @param amount0 The amount of token0
-    /// @param amount1 The amount of token1
-
-    function _sendToOwner(
-        uint256 tokenId,
-        uint256 amount0,
-        uint256 amount1
-    ) internal {
-        // get owner of contract
-        address owner = deposits[tokenId].owner;
-
-        address token0 = deposits[tokenId].token0;
-        address token1 = deposits[tokenId].token1;
-        // send collected fees to owner
-        TransferHelper.safeTransfer(token0, owner, amount0);
-        TransferHelper.safeTransfer(token1, owner, amount1);
-    }
-        
-        /// @notice Increases liquidity in the current range
+    /// @notice Increases liquidity in the current range
     /// @dev Pool must be initialized already to add liquidity
     /// @param tokenId The id of the erc721 token
     /// @param amount0 The amount to add of token0
@@ -223,22 +187,46 @@ contract LiquidityExamples is IERC721Receiver {
             uint128 liquidity,
             uint256 amount0,
             uint256 amount1
-        )
-    {
-        INonfungiblePositionManager.IncreaseLiquidityParams memory params =
-            INonfungiblePositionManager.IncreaseLiquidityParams({
-                tokenId: tokenId,
-                amount0Desired: amountAdd0,
-                amount1Desired: amountAdd1,
-                amount0Min: 0,
-                amount1Min: 0,
-                deadline: block.timestamp
-            });
+        ) {
+
+        TransferHelper.safeTransferFrom(deposits[tokenId].token0, msg.sender, address(this), amountAdd0);
+        TransferHelper.safeTransferFrom(deposits[tokenId].token1, msg.sender, address(this), amountAdd1);
+
+        TransferHelper.safeApprove(deposits[tokenId].token0, address(nonfungiblePositionManager), amountAdd0);
+        TransferHelper.safeApprove(deposits[tokenId].token1, address(nonfungiblePositionManager), amountAdd1);
+
+        INonfungiblePositionManager.IncreaseLiquidityParams memory params = INonfungiblePositionManager.IncreaseLiquidityParams({
+            tokenId: tokenId,
+            amount0Desired: amountAdd0,
+            amount1Desired: amountAdd1,
+            amount0Min: 0,
+            amount1Min: 0,
+            deadline: block.timestamp
+        });
 
         (liquidity, amount0, amount1) = nonfungiblePositionManager.increaseLiquidity(params);
+
     }
 
-    
+    /// @notice Transfers funds to owner of NFT
+    /// @param tokenId The id of the erc721
+    /// @param amount0 The amount of token0
+    /// @param amount1 The amount of token1
+    function _sendToOwner(
+        uint256 tokenId,
+        uint256 amount0,
+        uint256 amount1
+    ) internal {
+        // get owner of contract
+        address owner = deposits[tokenId].owner;
+
+        address token0 = deposits[tokenId].token0;
+        address token1 = deposits[tokenId].token1;
+        // send collected fees to owner
+        TransferHelper.safeTransfer(token0, owner, amount0);
+        TransferHelper.safeTransfer(token1, owner, amount1);
+    }
+
     /// @notice Transfers the NFT to the owner
     /// @param tokenId The id of the erc721
     function retrieveNFT(uint256 tokenId) external {
