e3e9021d40a56044f995f9de05614a671cae7ee4
Rio <onwukachibike@gmail.com>
2023-03-10 14:32:59 +0100

    updated'

--- contracts/SwapExamples.sol
+++ contracts/SwapExamples.sol
@@ -5,7 +5,7 @@ pragma abicoder v2;
 import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 
-contract SingleSwapExample{
+contract SwapExamples{
     // For the scope of these swap examples,
     // we will detail the design considerations when using
     // `exactInput`, `exactInputSingle`, `exactOutput`, and  `exactOutputSingle`.
@@ -20,6 +20,7 @@ contract SingleSwapExample{
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address public constant JJJ= 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     // For this example, we will set the pool fee to 0.3%.
     uint24 public constant poolFee = 3000;
--- test/swap.test.js
+++ test/swap.test.js
@@ -7,48 +7,35 @@ const USDC ="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
 
 
 describe("SwapExamples", () => {
-    let swapExamples
-    let accounts
-    let weth
-    let dai
-    let usdc
-  
-    before(async () => {
-      accounts = await ethers.getSigners(1)
-  
-      const SwapExamples = await ethers.getContractFactory("SingleSwapExample.sol")
-      swapExamples = await SwapExamples.deploy()
-      await swapExamples.deployed()
-  
-      weth = await ethers.getContractAt("IWETH", WETH9)
-      dai = await ethers.getContractAt("IERC20", DAI)
-      usdc = await ethers.getContractAt("IERC20", USDC)
-    })
-  
-    it("swapExactInputSingle", async () => {
-      const amountIn = 10n ** 18n
-  
-      // Deposit WETH
-      await weth.deposit({ value: amountIn })
-      await weth.approve(swapExamples.address, amountIn)
-  
-      // Swap
-      await swapExamples.swapExactInputSingle(amountIn)
-  
-      console.log("DAI balance", await dai.balanceOf(accounts[0].address))
-    })
-  
-    it("swapExactOutputSingle", async () => {
-      const wethAmountInMax = 10n ** 18n
-      const daiAmountOut = 100n * 10n ** 18n
-  
-      // Deposit WETH
-      await weth.deposit({ value: wethAmountInMax })
-      await weth.approve(swapExamples.address, wethAmountInMax)
-  
-      // Swap
-      await swapExamples.swapExactOutputSingle(daiAmountOut, wethAmountInMax)
-  
-      console.log("DAI balance", await dai.balanceOf(accounts[0].address))
-    })
+  let swapExamples
+  let accounts
+  let weth
+  let dai
+  let usdc
+
+  before(async () => {
+    accounts = await ethers.getSigners(1)
+
+    const SwapExamples = await ethers.getContractFactory("SwapExamples")
+    swapExamples = await SwapExamples.deploy()
+    await swapExamples.deployed()
+
+    weth = await ethers.getContractAt("IWETH", WETH9)
+    dai = await ethers.getContractAt("IERC20", DAI)
+    usdc = await ethers.getContractAt("IERC20", USDC)
+  })
+
+  it("swapExactInputSingle", async () => {
+    const amountIn = 10n ** 18n
+
+    // Deposit WETH
+    await weth.deposit({ value: amountIn })
+    await weth.approve(swapExamples.address, amountIn)
+
+    // Swap
+    await swapExamples.swapExactInputSingle(amountIn)
+
+    console.log("DAI balance", await dai.balanceOf(accounts[0].address))
+  })
+
 })
