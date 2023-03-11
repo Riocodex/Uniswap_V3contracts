e4ec3add92ecc4dba377e73465f06c9250b5c420
Rio <onwukachibike@gmail.com>
2023-03-11 21:02:41 +0100

    updated

--- contracts/SwapExamples.sol
+++ contracts/SwapExamples.sol
@@ -6,7 +6,7 @@ import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
 
 contract SwapExamples{
-    =
+    
 
     ISwapRouter public constant swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
 
@@ -16,21 +16,20 @@ contract SwapExamples{
     address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public constant JJJ= 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
-
+  
     uint24 public constant poolFee = 3000;
 
    
 
- 
     
     function swapExactInputSingle(uint256 amountIn) external returns (uint256 amountOut) {
-       
+        
         TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountIn);
 
        
         TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
 
-    
+        
         ISwapRouter.ExactInputSingleParams memory params =
             ISwapRouter.ExactInputSingleParams({
                 tokenIn: WETH9,
@@ -43,16 +42,18 @@ contract SwapExamples{
                 sqrtPriceLimitX96: 0
             });
 
-       
+        
         amountOut = swapRouter.exactInputSingle(params);
     }
 
+ 
 
 
     function swapExactOutputSingle(uint256 amountOut, uint256 amountInMaximum) external returns (uint256 amountIn) {
+       
         TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountInMaximum);
 
-        
+      
         TransferHelper.safeApprove(WETH9, address(swapRouter), amountInMaximum);
 
         ISwapRouter.ExactOutputSingleParams memory params =
@@ -70,7 +71,7 @@ contract SwapExamples{
         
         amountIn = swapRouter.exactOutputSingle(params);
 
-        
+       
         if (amountIn < amountInMaximum) {
             TransferHelper.safeApprove(WETH9, address(swapRouter), 0);
             TransferHelper.safeTransfer(WETH9, msg.sender, amountInMaximum - amountIn);
