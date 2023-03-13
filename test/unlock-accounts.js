db9b8321138bf5c1f31631db425c5e7840c8b903
Rio <onwukachibike@gmail.com>
2023-03-13 17:47:03 +0100

    updated

--- test/unlock-accounts.js
+++ test/unlock-accounts.js
@@ -2,7 +2,7 @@ const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
 const DAI ="0x6B175474E89094C44Da98b954EedeAC495271d0F";
-
+const DAI_WHALE="0xF977814e90dA44bFA03b6295A0616a897441aceC"
 
 
 describe("SwapExamples", () => {
@@ -14,6 +14,11 @@ describe("SwapExamples", () => {
 
   before(async () => {
     accounts = await ethers.getSigners()
+
+    await hre.network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: ["0xF977814e90dA44bFA03b6295A0616a897441aceC"]
+    })
 
     dai = await ethers.getContractAt("IERC20", DAI)
    
