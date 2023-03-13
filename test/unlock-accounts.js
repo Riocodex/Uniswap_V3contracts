7e778c3fe59a664873d41c7266109929d0ea0f58
Rio <onwukachibike@gmail.com>
2023-03-13 17:19:44 +0100

    updated

--- test/unlock-accounts.js
+++ test/unlock-accounts.js
@@ -2,16 +2,15 @@ const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
 const DAI ="0x6B175474E89094C44Da98b954EedeAC495271d0F";
-const WETH9 ="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-const USDC ="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+
 
 
 describe("SwapExamples", () => {
-  let swapExamples
+ 
   let accounts
-  let weth
+  let whale
   let dai
-  let usdc
+
 
   before(async () => {
     accounts = await ethers.getSigners(1)
